671b896c2afbf02dc32459cf10f825324d8c3236
Joe DePung <joe.depung@gmail.com>
2014-04-11 07:38:53 -0500

    adds initial profile (my2q) page - Joe

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Y2q::Application.routes.draw do
   get "about/index"
+  resource :profile, only: ['show']
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   root 'welcome#index'
 end
