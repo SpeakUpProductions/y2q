0ecf24e408ead391676dae5c17fae5a110e56de0
Joe DePung <joe.depung@gmail.com>
2014-04-14 07:14:31 -0500

    12 - clearing out old schema.rb data (Joe)

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,6 @@ ActiveRecord::Schema.define(version: 20140331045801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tests", force: true do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
