29594ee5bbf63edaa25f384e37f872371f0fa306
Joe DePung <joe.depung@gmail.com>
2014-04-14 07:59:31 -0500

    12 - adds ability to add new video url to db

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,5 +3,19 @@ class VideosController < ApplicationController
   end
 
   def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      redirect_to profile_path
+    else
+      render :new
+    end
+  end
+
+  def video_params
+    params.require(:video).permit(:title, :embed_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Y2q::Application.routes.draw do
   get "about/index"
-  resources :videos, only: [:index, :new]
+  resources :videos, only: [:index, :new, :create]
   resource :profile, only: ['show']
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   root 'welcome#index'
