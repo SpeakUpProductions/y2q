968252b28b1d5ce11c619a9d1ff0e1f4a494092b
Joe DePung <joe.depung@gmail.com>
2014-04-15 08:50:18 -0500

    11 [Code Review] - doing authentication the baked in devise way (Joe)

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,14 +1,12 @@
 class VideosController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
+
   def index
     @heartbreaks = Heartbreak.all
     @inspirations = Inspiration.all
   end
 
   def new
-    if !user_signed_in?
-    redirect_to new_user_session_path
-    end
-
     @video = Video.new
     @heartbreaks = Heartbreak.all
     @inspirations = Inspiration.all
