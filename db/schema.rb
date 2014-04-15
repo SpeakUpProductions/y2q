7b85832c535b3b33912a86ea22908791c66802dc
Ben Pomerenke <bpomerenke@gmail.com>
2014-04-14 20:15:28 -0500

    11 - added inspirations - BenP

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140415011004) do
+ActiveRecord::Schema.define(version: 20140415011441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "heartbreaks", force: true do |t|
+    t.string   "display"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inspirations", force: true do |t|
     t.string   "display"
     t.datetime "created_at"
     t.datetime "updated_at"
