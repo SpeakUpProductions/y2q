6e1a312830c74da6c2a0a568b7bbc533fb027a49
Joe DePung <joe.depung@gmail.com>
2014-04-29 22:09:45 -0500

    30 - adds organizations table (Joe)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416041119) do
+ActiveRecord::Schema.define(version: 20140430030754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20140416041119) do
     t.string   "display"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "organizations", force: true do |t|
+    t.integer "profile_id"
+    t.string  "name"
+    t.string  "website"
+    t.integer "org_type"
   end
 
   create_table "profiles", force: true do |t|
