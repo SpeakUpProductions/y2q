0e5d1741e66e13ee6c40cdcd05f8c076f9a10b31
bpom <bpomerenke@gmail.com>
2014-05-10 23:05:52 -0500

    32 - hack stab at grabbing thumb url - BenP

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,15 @@
+require 'net/http'
+require 'uri'
+
 module VideosHelper
+    def open(url)
+      Net::HTTP.get(URI.parse(url))
+    end
+    def get_thumb_url(embed_url)
+    	google_id = embed_url.sub 'https://www.youtube.com/watch?v=', ''
+	page_content = open('http://gdata.youtube.com/feeds/api/videos/'+google_id)
+	url_and_beyond = page_content.partition("media:thumbnail url='").last
+        just_url = url_and_beyond.partition("'").first
+	return just_url
+    end
 end
