76c594f6823a439865da396cd8bc35801814f0e7
bpom <bpomerenke@gmail.com>
2014-06-05 00:18:01 -0500

    58 - added js from demo to make video slide up when finished - BenP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,68 @@
 //= require_tree .
 //= require fonts
 //= require isotope
+//= require jquery.fitvids.js
 
-$(function(){ $(document).foundation(); });
+$(function(){ 
+  $("#videoheader").fitVids();	
+  var f = $('iframe');
+  var url = f.attr('src').split('?')[0];
+	
+  // Listen for messages from the player
+  if (window.addEventListener){
+    window.addEventListener('message', onMessageReceived, false);
+  }
+  else {
+    window.attachEvent('onmessage', onMessageReceived, false);
+  }
+	
+  // Handle messages received from the player
+  function onMessageReceived(e) {
+    var data = JSON.parse(e.data);
+    
+    switch (data.event) {
+        case 'ready':
+            onReady();
+            break;
+                      
+        case 'finish':
+            onFinish();
+            break;
+    }
+  }
+                   	
+  // Helper function for sending a message to the player
+  function post(action, value) {
+    var data = { method: action };
+    
+    if (value) {
+        data.value = value;
+    }
+    
+    f.each(function(){
+      var sData = JSON.stringify(data);
+      this.contentWindow.postMessage(sData, url);
+    });
+  }
+
+  // Play & Pause 
+  $('.js-container a').click(function() {
+    if ($("#videoheader").length > 0){
+      post('pause');
+      $("#videoheader").remove();
+    }
+  });
+		
+	
+  function onReady() {
+    post('addEventListener', 'finish');
+  }
+
+  function onFinish() {
+    $("#videoheader").slideUp(1500, function() {
+      $("#videoheader").remove();
+    });
+  }
+  $(document).foundation(); 
+
+});
