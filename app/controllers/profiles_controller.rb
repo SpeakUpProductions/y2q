e3398577592b5603bb42475c66f6484b1b0f5681
Joe DePung <joe.depung@gmail.com>
2014-07-04 15:57:19 -0500

    67 - using decent exposure for profile stuff (Joe)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,26 +1,21 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_profile, only: [:show, :edit, :update]
+  expose(:videos) { current_user.videos }
+  expose(:profile) { current_user.profile }
 
   def edit
-    @profile = current_user.profile
   end
 
   def show
-    @videos = current_user.videos
   end
 
   def update
-    if @profile.update(profile_params)
+    if profile.update(profile_params)
       redirect_to profile_url, notice: 'Profile successfully updated.'
     end
   end
 
   private
-
-  def set_profile
-    @profile = Profile.find(current_user.profile.id)
-  end
 
   def profile_params
     params.require(:profile).permit(
