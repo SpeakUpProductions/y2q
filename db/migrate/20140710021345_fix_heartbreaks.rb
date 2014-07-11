ab244ced242b296d80ccdec2c4d0ddb201ba7ae5
Joe DePung <joe.depung@gmail.com>
2014-07-10 21:17:00 -0500

    71 [Code Review] - fixing mistakes in migration file.  Additionally changed order to match the list DP gave (Joe)

--- db/migrate/20140710021345_fix_heartbreaks.rb
+++ db/migrate/20140710021345_fix_heartbreaks.rb
@@ -2,24 +2,24 @@ class FixHeartbreaks < ActiveRecord::Migration
 
   def down
     Heartbreak.delete_all()
-    Heartbreak.create(display_text: "Education")
     Heartbreak.create(display_text: "Human Rights")
-    Heartbreak.create(display_text: "Nutrition & Water Crisis")
-    Heartbreak.create(display_text: "Physical/Mental Health")
     Heartbreak.create(display_text: "Poverty")
-    Heartbreak.create(display_text: "Violence/Abuse")
+    Heartbreak.create(display_text: "Animal/Environment")
     Heartbreak.create(display_text: "War/Genocide")
+    Heartbreak.create(display_text: "Health")
+    Heartbreak.create(display_text: "Education")
+    Heartbreak.create(display_text: "Violence/Abuse")
   end
 
   def up
-   Heartbreak.delete_all()
-   Heartbreak.create(display_text: "Animal/Environment")
-   Heartbreak.create(display_text: "Education")
-   Heartbreak.create(display_text: "Health")
-   Heartbreak.create(display_text: "Human Rights")
-   Heartbreak.create(display_text: "Poverty")
-   Heartbreak.create(display_text: "Violence/Abuse")
-   Heartbreak.create(display_text: "War/Genocide")
+    Inspiration.delete_all()
+    Inspiration.create(display_text: "Politics")
+    Inspiration.create(display_text: "Education")
+    Inspiration.create(display_text: "Activism")
+    Inspiration.create(display_text: "Arts & Culture")
+    Inspiration.create(display_text: "Humanitarian")
+    Inspiration.create(display_text: "Business")
+    Inspiration.create(display_text: "Science")
   end
 
 end
