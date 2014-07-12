3d50ec0cb78144768c89c14b5f3646ca02525fdd
Joe DePung <joe.depung@gmail.com>
2014-07-11 23:44:11 -0500

    123 - regular expression to save url without protocol or leading slashes (Joe)

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,4 +3,18 @@ class Organization < ActiveRecord::Base
 
   belongs_to :profile
   validates :profile_id, presence: true
+
+  def website=(new_website)
+    write_attribute(:website, extract_url(new_website))
+  end
+
+  private
+  def extract_url(url)
+    matches = url_expression.match(url)
+    matches.blank? ? "" : matches[:address_no_protocol]
+  end
+
+  def url_expression
+    /^((http:|https:|)\/\/|)(?<address_no_protocol>.+)/i
+  end
 end
