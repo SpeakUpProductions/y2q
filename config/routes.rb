5c986c4423a3d8e66b9b5e865b27bc917e6f5b19
Joe DePung <joe.depung@gmail.com>
2014-07-16 01:17:33 -0500

    99 - creating instructions controller and moving stuff (Joe)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Y2q::Application.routes.draw do
   ActiveAdmin.routes(self)
   get "about/index"
   get "videos/filtered", as: 'filtered_videos'
+  get "instructions", as: :instructions, to: 'instructions#show'
   resources :videos
   resource :profile, only: ['edit', 'show', 'update'], as: 'current_profile' do
     get "videos", to: "videos#current_user_index", as: "current_user_videos"
