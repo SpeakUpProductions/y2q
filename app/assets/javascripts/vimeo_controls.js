74b49b6d6e13766a1d852f5bb8f059971e67c3b6
bpom <bpomerenke@gmail.com>
2014-09-30 23:20:22 -0500

    a3 - dont wire up video controls unless the iframe is on the page - BP

--- app/assets/javascripts/vimeo_controls.js
+++ app/assets/javascripts/vimeo_controls.js
@@ -1,22 +1,25 @@
 (function() {
   $(function(){
-    var iframe = $('#home_video')[0];
-    var player = $f(iframe);
-    var flexVideo = $('.flex-video.vimeo');
+    if($('#home_video').length > 0)
+    {
+      var iframe = $('#home_video')[0];
+      var player = $f(iframe);
+      var flexVideo = $('.flex-video.vimeo');
 
-    // When the player is ready, add listeners
-    player.addEvent('ready', function() {
-        player.addEvent('finish', onFinish);
-    });
-
-    function onFinish(id) {
-      flexVideo.slideUp(1500, function() {
-        flexVideo.remove();
-        $.ajax({
-          url : 'welcome/played',
-          type : 'PUT'
-        });
+      // When the player is ready, add listeners
+      player.addEvent('ready', function() {
+          player.addEvent('finish', onFinish);
       });
+
+      function onFinish(id) {
+        flexVideo.slideUp(1500, function() {
+          flexVideo.remove();
+          $.ajax({
+            url : 'welcome/played',
+            type : 'PUT'
+          });
+        });
+      }
     }
   })
 }())
