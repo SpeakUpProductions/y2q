71c06a3672785abd3a309d2343ded02a3b8423b3
bpom <bpomerenke@gmail.com>
2014-10-11 22:12:04 -0500

    b1 - save developer on submission - BP

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,8 +1,18 @@
 class DevelopersController < ApplicationController
   expose(:developer) 
+  expose(:message) 
   def new
     self.developer = Developer.new
   end
   def create
+    developer = Developer.new(developer_params)
+    if developer.save
+      self.message = "Thank you for registering as a developer"
+    else
+      render :new
+    end
+  end
+  def developer_params
+    params.require(:developer).permit(:name,:email)
   end
 end
