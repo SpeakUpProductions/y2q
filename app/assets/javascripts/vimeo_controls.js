2a1d91b886a907999aa9640cd6233057c4af48f7
Joe DePung <joe.depung@gmail.com>
2014-11-09 18:16:09 -0600

    [Code Review] Remove unused events

--- app/assets/javascripts/vimeo_controls.js
+++ app/assets/javascripts/vimeo_controls.js
@@ -6,9 +6,7 @@
 
     // When the player is ready, add listeners for pause, finish, and playProgress
     player.addEvent('ready', function() {
-        player.addEvent('pause', onPause);
         player.addEvent('finish', onFinish);
-        player.addEvent('playProgress', onPlayProgress);
     });
 
     // Call the API when a button is pressed
@@ -16,19 +14,10 @@
         player.api($(this).text().toLowerCase());
     });
 
-    function onPause(id) {
-      //do nothing
-    }
-
     function onFinish(id) {
       flexVideo.slideUp(1500, function() {
         flexVideo.remove();
       });
-    }
-
-    function onPlayProgress(data, id) {
-        // example of what to do in case we need it
-        // status.text(data.seconds + 's played');
     }
   })
 }())
